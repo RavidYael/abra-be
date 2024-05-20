62af9c5d27372c5fad3173e680c51f1eed29e906
RavidYael <ravidyael.githubuser@gmail.com>
2024-05-20 13:34:57 +0300

    feat(api): pets get and post endpoints

---
+++ abra-be/Controllers/PetsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace abra_be.Controllers
+{
+    [Route("api/pet")]
+    [ApiController]
+    public class PetsController : ControllerBase
+    {
+        // GET: api/<PetsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<PetsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
