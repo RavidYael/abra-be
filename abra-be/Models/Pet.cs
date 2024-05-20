6fa7d7e8ee5dc9f8f3e8743da35d076a2ba08989
RavidYael <ravidyael.githubuser@gmail.com>
2024-05-20 13:48:28 +0300

    feat: initial pet model

---
+++ abra-be/Models/Pet.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace abra_be.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public PetType Type { get; set; }
+        
+        public enum PetType
+        {
+            Dog,
+            Cat,
+            Horse,
+            Other
+        }
+        
+        [Required]
+        public string Color { get; set; }
+        
+        [Required]
+        public int Age { get; set; }
+    }
+}
